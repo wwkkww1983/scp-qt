38b73d51ae0462f44098e0af2b95a2651f32f566
Karl Wisdom <k.j.hirner.wisdom@gmail.com>
2019-01-29 06:27:02 -0800

    updates to config dialog, updates to main window

--- lib_widget/entryView.py
+++ lib_widget/entryView.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/entryView.ui'
+# Form implementation generated from reading ui file './ui/entryView.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
--- lib_widget/scp_qt_tray.py
+++ lib_widget/scp_qt_tray.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/scp_qt_tray.ui'
+# Form implementation generated from reading ui file './ui/scp_qt_tray.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
